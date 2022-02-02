6c219f139e1df1cba87357b1936f228ced394fc5
Felipa Mendez <felipalouise@gmail.com>
2022-02-01 23:56:33 -0600

    commit tree 7102590fca210cf2ed05981ae7a78d4a9d30ae48

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import EyesOnMe from "./EyesOnMe"
+
+function changeHandler() {
+    console.log("Entering password...")
+   }
+
+function Keypad() {
+    return (
+        <>
+            <input type="password" onChange={changeHandler}/>
+        </>
+    )
+}
+export default Keypad
